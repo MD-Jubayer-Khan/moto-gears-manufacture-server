4eaa2679729305de64f1b2bc98260f9ac4480dd3
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-22 22:05:06 +0600

    connect server with mongodb database

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.my6ni.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect()
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.my6ni.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect()
 
-// async function run (){
-//     try{
-//         await client.connect();
-//         const partsCollection = client.db('moto_gears').collection('parts')
-//     }
+async function run (){
+    try{
+        await client.connect();
+        const partsCollection = client.db('moto_gears').collection('parts')
+    }
 
-//     finally{
+    finally{
 
-//     }
-// };
-// run().catch(console.dir)
+    }
+};
+run().catch(console.dir)
 
 app.get('/', (req, res)=> {
     res.send('Hello from moto gears')
